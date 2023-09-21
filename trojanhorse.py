bbfa1bb8b731dca9aba568f49c2e8ff4fa110030
Mark Victor Kithinji <135356007+VictorCodebase@users.noreply.github.com>
2023-09-21 03:54:00 +0300

    Delete trojanhorse.py
    unnecessary

--- trojanhorse.py
+++
@@ -1,9 +0,0 @@
-
-class raven:
-    def __init__(self, contents, keyword):
-        self.contents = contents
-        self.keyword = keyword
-        self.send()
-    
-    def send(self):
-        print(f"the raven successfully sent the contents: {self.contents} with hopes its about: {self.keyword}")
